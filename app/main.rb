37db9be4be054a6126b2e19c2b4dd7fc28e18a00
Takeo Asai <spitson+github@gmail.com>
2016-02-27 16:55:46 +0900

    action 追加

--- app/main.rb
+++ app/main.rb
@@ -1,11 +1,28 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-require_relative "./hatenabookmark.rb"
+require_relative "models/HBookmark"
+require_relative "models/LWeather"
 
 get "/" do
   @title = "main"
 
+  erb :index
+end
+
+get "/weather" do
+  @title = "weather"
+
+  LWeather.report.slack
+
+  erb :index
+end
+
+get "/hatena" do
+  @title = "hatena bookmark"
+
+  HBookmark.stockByTag("ライフハック")
+  HBookmark.unslacked.first.slack
 
   erb :index
 end
